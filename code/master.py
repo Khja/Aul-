ae15ed71c27cabe6b72f2a74c9aba9472a73290e
Khja <34775710+Khja@users.noreply.github.com>
2020-12-28 21:12:43 +0100

    Update master.py

--- code/master.py
+++ code/master.py
@@ -2,6 +2,43 @@ import model as md
 import data as dt
 from PySide2 import QtWidgets
 
+class Master():
+    def __init__(self, filename, gui):
+        self._database = dt.Database(filename)
+        self._gui = gui
+        self.trees = ('_morp', '_dict', '_temp', '_symb', '_rule', '_tabl')
+        # create model for each model in Database
+        for model in self.trees:
+            setattr(self, model, md.Model(self._database, model))
+        # create model for table names
+        self.table_rules = []
+        self.tables = md.Model(self._database, '_alltables')
+        self._stack = QtWidgets.QUndoStack()
+
+    def addChild(self, node, obj_type, model_name, parent=None):
+        model = getattr(self, model_name)
+        command = Add(node, obj_type, parent, model)
+        self._stack.push(command)
+        self._gui.ui.actionLbl.setText(command._redoMessage)
+
+    def removeChild(self, index, model_name):
+        model = getattr(self, model_name)
+        command = Remove(index, model)
+        self._stack.push(command)
+        self._gui.ui.actionLbl.setText(command._redoMessage)
+
+    def editData(self, index, model_name, new_data, new_name):
+        model = getattr(self, model_name)
+        node = index.internalPointer()
+        old_data = {}
+        for d in new_data:
+            if d in node._data:
+                old_data[d] = node._data[d]
+            else:
+                old_data[d] = None
+        command = Edit(index, [node._name, new_name], [old_data, new_data], model)
+        self._stack.push(command)
+
 class Add(QtWidgets.QUndoCommand):
     def __init__(self, node, obj_type, parent, model):
         super(Add, self).__init__()
@@ -66,37 +103,3 @@ class Edit(QtWidgets.QUndoCommand):
         self._model.editChild(self._index, self._old_name, self._old_data)
         self._model.layoutChanged.emit()
         self._undoMessage = f'Reverted data'
-
-class Master():
-    def __init__(self, filename, gui):
-        self._database = dt.Database(filename)
-        self._gui = gui
-        self.trees = ('_morp', '_dict', '_temp', '_symb', '_rule', '_tabl')
-        # create model for each model in Database
-        for model in self.trees:
-            setattr(self, model, md.Model(self._database, model))
-        self._stack = QtWidgets.QUndoStack()
-
-    def addChild(self, node, obj_type, model_name, parent=None):
-        model = getattr(self, model_name)
-        command = Add(node, obj_type, parent, model)
-        self._stack.push(command)
-        self._gui.ui.actionLbl.setText(command._redoMessage)
-
-    def removeChild(self, index, model_name):
-        model = getattr(self, model_name)
-        command = Remove(index, model)
-        self._stack.push(command)
-        self._gui.ui.actionLbl.setText(command._redoMessage)
-
-    def editData(self, index, model_name, new_data, new_name):
-        model = getattr(self, model_name)
-        node = index.internalPointer()
-        old_data = {}
-        for d in new_data:
-            if d in node._data:
-                old_data[d] = node._data[d]
-            else:
-                old_data[d] = None
-        command = Edit(index, [node._name, new_name], [old_data, new_data], model)
-        self._stack.push(command)
